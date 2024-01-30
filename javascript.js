8338e03e57739c9e03305daa357b835b25e2245d
Nontas_g <Nontasgeorgoulias@gmail.com>
2024-01-29 22:05:57 -0500

    Created playRound function, and player and computer selection

--- javascript.js
+++ javascript.js
@@ -15,3 +15,27 @@ function getComputerChoice() {
             break;
     }
 }
+
+const playerSelection = prompt().toLowerCase();
+
+const computerSelection = getComputerChoice();
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return "It's a tie!";
+    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        return "Loser! Paper beats Rock!";
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        return "Loser! Scissors beats Paper!";
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        return "Loser! Rock beats Scissors!";
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        return "Winner! Rock beats Scissors!";
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        return "Winner! Paper beats Rock!";
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        return "Winner! Scissors beats Paper!";
+    } else {
+        return "skill issue";
+    }
+}
